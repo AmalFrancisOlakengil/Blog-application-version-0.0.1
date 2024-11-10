88ea01121599f890cfe6a29751a70eb460c24d01
AmalFrancisOlakengil <francisamal030@gmail.com>
2024-11-10 19:24:28 +0530

    Cors update

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
+CORS(app)
+CORS(app, origins=["https://blog-application-version-0-0-1.vercel.app"])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
